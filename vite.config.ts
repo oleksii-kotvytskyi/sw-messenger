60810fb3513c21e37e28b8a9997c59e4e61a2ccb
Oleksii Kotvytskyi <kot.aleksei43@gmail.com>
2024-01-12 13:19:43 +0100

    feat: setup project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
 });
